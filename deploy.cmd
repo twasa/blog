b6025ff7bd862cfe42d58ec0d017987f2fa6e14e
twasa <twasawang@gmail.com>
2018-01-06 22:25:54 +0800

    Hugo update 2018-01-06 22:25:54

--- deploy.cmd
+++ deploy.cmd
@@ -1,10 +1,10 @@
 set workdir=%~dp0
 set current=%date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2%
 cd %workdir%
-git add .
+git add -A
 git commit -m "Hugo update %current%"
 git push -u origin master
-hugo --buildDrafts
+.\hugo.exe --buildDrafts
 cd public
 git add .
 git commit -m "Blog update %current%"
